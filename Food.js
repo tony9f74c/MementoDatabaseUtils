ec256055eb00733191555134ab559d6f7d5203c9
Tony <11808030+tony9f74c@users.noreply.github.com>
2025-11-09 22:11:02 +0200

    Update Food.js

--- Food.js
+++ Food.js
@@ -6,21 +6,26 @@ function parsePerUnit(value) {
 }
 
 function updateCosts(currentLib, transactionsLib) {
-    let records         = currentLib.entries();
-    let transactions    = transactionsLib.entries();
-
+    let records      = currentLib.entries();
+    let transactions = transactionsLib.entries();
     records.forEach(record => {
-        const currentItem = record.field('Item');
+        const currentItem  = record.field('Item');
+        const currentBrand = record.field('Brand');
+        const currentDetails = record.field('Details');
         const filtered = transactions
-            .filter(t => t.field('Item') === currentItem) // Match "Item"
-        .sort((a, b) => new Date(b.field('Date')) - new Date(a.field('Date'))) // newest first
-        .slice(0, 5); // Keep latest 5
+            .filter(t =>
+                t.field('Item') === currentItem &&
+                t.field('Brand') === currentBrand &&
+                t.field('Details') === currentDetails
+            )
+            .sort((a, b) => new Date(b.field('Date')) - new Date(a.field('Date'))) // newest first
+            .slice(0, 5); // latest 5
         const sum = filtered.reduce(
-        (acc, t) => acc + parsePerUnit(t.field('Per Unit')),
+            (acc, t) => acc + parsePerUnit(t.field('Per Unit')),
             0
         );
         const avg = filtered.length ? sum / filtered.length : 0;
-        record.set('Cost (100g)', (avg/10).toFixed(2));
+        record.set('Cost (100g)', (avg / 10).toFixed(2));
     });
 }
 
